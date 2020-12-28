116c51944a369fd256758ca74c6a9d7a5f5b781c
Band <bandi@oregonstate.edu>
2020-12-28 01:09:24 -0800

    inputter 2nd draft

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main( int argc, char *argv[] )
 
 				// Get player input
 				ControllerStatus* status_buffer = gca::Process();
-				std::cout << status_buffer[0] << std::endl;
+				//std::cout << status_buffer[0] << std::endl;
 				
 				//std::cout << gca::RawData() << std::endl; 
 				// Compute next game state
